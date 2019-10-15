97844768706e4c89f2895ea641c52a6c76e06ad7
Mohamed Mansour <sphone.developer@gmail.com>
2019-10-15 17:39:58 +0200

    add pods

---
+++ Podfile
@@ -0,0 +1,27 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'iWatchPN' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for iWatchPN
+
+end
+
+target 'iWatchPN WatchKit App' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for iWatchPN WatchKit App
+ pod 'Firebase/Messaging'
+
+end
+
+target 'iWatchPN WatchKit Extension' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+ 	pod 'Moya', '~> 11.0'
+  # Pods for iWatchPN WatchKit Extension
+
+end
--- iWatchPN WatchKit Extension/InterfaceController.swift
+++ iWatchPN WatchKit Extension/InterfaceController.swift
@@ -12,9 +12,52 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var userNameTxt: WKInterfaceLabel!
+    var userName : String = ""
+    
+    @IBAction func numberOneBtnAction() {
+        setUserName(char: "1")
+    }
+    @IBAction func numberTwoBtnAction() {
+        setUserName(char: "2")
+    }
+    @IBAction func numberThreeBtnAction() {
+         setUserName(char: "3")
+    }
+    @IBAction func numberFourBtnAction() {
+         setUserName(char: "4")
+    }
+    @IBAction func numberFiveBtnAction() {
+         setUserName(char: "5")
+    }
+    @IBAction func numberSixBtnAction() {
+         setUserName(char: "6")
+    }
+    @IBAction func numberSevenBtnAction() {
+         setUserName(char: "7")
+    }
+    @IBAction func numberEightBtnAction() {
+         setUserName(char: "8")
+    }
+    @IBAction func numberNineBtnAction() {
+         setUserName(char: "9")
+    }
+    @IBAction func NumberZeroBtnAction() {
+         setUserName(char: "0")
+    }
+    @IBAction func clearBtnAction() {
+         setUserName(char: "")
+    }
+    
+    func setUserName(char : String)
+    {
+       userName =  char != "" ? (userName + char) : ""
+       userNameTxt.setText(userName)
+    }
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
+    
         // Configure interface objects here.
     }
     
